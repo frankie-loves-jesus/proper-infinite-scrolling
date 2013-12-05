64a2a362e3d2b21a3c862b225aa64640468a93d1
dev <dev@dev.my.domain>
2013-12-05 02:59:04 +0100

    Generate Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProperInfiniteScrolling::Application.config.secret_token = 'bc199efb825667cd9089817521a40e2912d578336f07bea910dbecea3ae87d4a72f4e713a68b8b704397737ee618634c78693aed49a235f01e874263d86eefe4'
