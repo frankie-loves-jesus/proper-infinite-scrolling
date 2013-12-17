fbd1a550e1728a9dfa4579eb8512201459345c63
dev <dev@dev.my.domain>
2013-12-17 08:37:05 +0100

    Add example from Ryan Ogden

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,13 +5,20 @@
 //= require_tree .
 
 $('table.topics').infiniteScrollHelper({
-  loadMore: function(page) {
-    // load some data, parse some data
-  },
+  loadMore: function(page, done) {
+    // use the page argument to find the next page to load. This assumes you have a list of links somewhere on the page for your html pagination
+    // jQuery eq filter is 0 based so we have to minus 1 from page to get the 0 based page to load
+    var url = $('.pagination a').eq(page - 1).attr('href'); // now we have the url to the next page of data
 
-  doneLoading: function() {
-    // return true if you are done doing your thing, false otherwise
-    return false;
+    $.get(url, function(data) {
+        // data will be the raw html from the url you just requested
+        // you can do what you need to with it, perhaps pull out just what you need and insert it into table.topics
+
+        console.log(data);
+
+        // now call the done callback to let the plugin know you are done loading
+        done();
+    });
   }
 });
 
