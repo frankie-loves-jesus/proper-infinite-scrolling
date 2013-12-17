db3c1c38ae7842ee67226e57bba5f177549541a1
dev <dev@dev.my.domain>
2013-12-17 09:28:15 +0100

    Bind JS to jQuery Mobile `pageshow`

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,21 +4,22 @@
 //= require jquery.infinite-scroll-helper.min
 //= require_tree .
 
-$('table.topics').infiniteScrollHelper({
-  loadMore: function(page, done) {
-    // use the page argument to find the next page to load. This assumes you have a list of links somewhere on the page for your html pagination
-    // jQuery eq filter is 0 based so we have to minus 1 from page to get the 0 based page to load
-    var url = $('.pagination a').eq(page - 1).attr('href'); // now we have the url to the next page of data
-
-    $.get(url, function(data) {
-        // data will be the raw html from the url you just requested
-        // you can do what you need to with it, perhaps pull out just what you need and insert it into table.topics
-
-        console.log(data);
-
-        // now call the done callback to let the plugin know you are done loading
-        done();
-    });
-  }
+$(document).on('pageshow', '.ui-page', function() {
+  $('table.topics').infiniteScrollHelper({
+    loadMore: function(page, done) {
+      // use the page argument to find the next page to load. This assumes you have a list of links somewhere on the page for your html pagination
+      // jQuery eq filter is 0 based so we have to minus 1 from page to get the 0 based page to load
+      var url = $('.pagination a').eq(page - 1).attr('href'); // now we have the url to the next page of data
+  
+      $.get(url, function(data) {
+          // data will be the raw html from the url you just requested
+          // you can do what you need to with it, perhaps pull out just what you need and insert it into table.topics
+  
+          console.log(data);
+  
+          // now call the done callback to let the plugin know you are done loading
+          done();
+      });
+    }
+  });
 });
-
