4e4b7eb9d3c54dcc477705ede2eff45819225df8
dev <dev@dev.my.domain>
2013-12-22 08:58:28 +0100

    check if it's incrementing correctly

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,10 @@ $(document).on('pageshow', '.ui-page', function() {
       // use the page argument to find the next page to load. This assumes you have a list of links somewhere on the page for your html pagination
       // jQuery eq filter is 0 based so we have to minus 1 from page to get the 0 based page to load
       var url = $('.pagination a').eq(page - 1).attr('href'); // now we have the url to the next page of data
-  
+
+      // check if it's incrementing correctly
+      console.log(page);  
+
       $.get(url, function(data) {
           // data will be the raw html from the url you just requested
           // you can do what you need to with it, perhaps pull out just what you need and insert it into table.topics
@@ -19,8 +22,9 @@ $(document).on('pageshow', '.ui-page', function() {
 
           // find and insert our fresh new content
           $(data).find('tr.topic').appendTo('table.topics'); 
- 
+
           // now call the done callback to let the plugin know you are done loading
+
           done();
       });
     }
