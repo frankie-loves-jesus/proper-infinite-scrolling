b09bc2bb61fb933d3403338317cf61e08ae57037
dev <dev@dev.my.domain>
2013-12-23 09:10:08 +0100

    we dont need eq()

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,8 +8,7 @@ $(document).on('pageshow', '.ui-page', function() {
   $('table.topics').infiniteScrollHelper({
     loadMore: function(page, done) {
       // use the page argument to find the next page to load. This assumes you have a list of links somewhere on the page for your html pagination
-      // jQuery eq filter is 0 based so we have to minus 1 from page to get the 0 based page to load
-      var url = $('.pagination a').eq(page - 1).attr('href'); // now we have the url to the next page of data
+      var url = $('.pagination a').attr('href'); // now we have the url to the next page of data
 
       // check if it's incrementing correctly
       console.log(page);  
@@ -18,10 +17,11 @@ $(document).on('pageshow', '.ui-page', function() {
           // data will be the raw html from the url you just requested
           // you can do what you need to with it, perhaps pull out just what you need and insert it into table.topics
   
-          // console.log(data);
+          //alert(data);
+console.log(data);
 
           // find and insert our fresh new content
-          $(data).find('tr.topic').appendTo('table.topics'); 
+          $(data).find('tr.topic').css('background', 'grey').appendTo('table.topics'); 
 
           // now call the done callback to let the plugin know you are done loading
 
